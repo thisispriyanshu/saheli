501ec0a81dd4192a58198cb2a70f12618e9652c3
Priyanshu Agrawal <priyanshuagrawal801@gmail.com>
2024-02-05 21:08:51 +0530

    Delete backend directory

--- backend/firebase/index.js
+++
@@ -1,7 +0,0 @@
-const admin = require("firebase-admin");
-const credentials = require("../serviceAccountKey.json");
-admin.initializeApp({
-  credential: admin.credential.cert(credentials),
-});
-
-module.exports = admin;
