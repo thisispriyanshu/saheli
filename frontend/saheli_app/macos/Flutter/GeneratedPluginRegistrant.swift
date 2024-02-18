d509a3fd420dc9b187c6d257c38d4414335ccd40
Apoorv yash <apoorvyash123@gmail.com>
2024-02-18 22:47:37 +0530

    added profile sos storage and recorder

--- frontend/saheli_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/saheli_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -22,6 +22,7 @@ import record_darwin
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
@@ -41,4 +42,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
